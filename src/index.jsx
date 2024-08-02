9aa1e93796a562340768eddd9ad8e8ff4924eab2
jaemin <wjdwoals9292@gmail.com>
2024-08-02 12:33:06 +0900

    feat: 쿠키사용 제거

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CookiesProvider } from 'react-cookie';
 import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CookiesProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </CookiesProvider>
   </React.StrictMode>
 );
 
