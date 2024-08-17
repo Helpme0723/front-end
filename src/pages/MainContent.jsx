a2628ee06498c3940a0ceaf82ed1bbc083e5e281
chominsoo <soochomin@gmail.com>
2024-08-17 19:13:13 +0900

    feat/guideLine

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -77,31 +77,117 @@ function MainContent() {
   return (
     <main className="main-content">
       <div className="mainheader">
-        <button onClick={() => setModalIsOpen(true)}>"사용 가이드"</button>
+        <button onClick={() => setModalIsOpen(true)}> 📚 </button>
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={() => setModalIsOpen(false)}
         >
           {/* 모달 페이지에 따른 콘텐츠 표시 */}
           {modalPage === 1 && (
-            <div>
+            <div
+              style={{
+                position: 'relative',
+                paddingBottom: '50px',
+                textAlign: 'center',
+              }}
+            >
+              <img
+                src="/favicon.ico"
+                alt="TalentVerse Favicon"
+                style={{
+                  width: '50px',
+                  height: '50px',
+                  display: 'block',
+                  margin: '0 auto',
+                }} // 중앙 정렬
+              />
               <h2>TalentVerse에 오신 여러분 환영합니다!</h2>
-              <p>여기는 첫 번째 페이지입니다.</p>
-              <button onClick={handleNextModalPage}>다음</button>
+              <p>저희 서비스의 간단한 사용법을 알려드릴게요!</p>
+              <button
+                onClick={handleNextModalPage}
+                style={{
+                  position: 'absolute',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                  bottom: '10px', // 모달 하단에서의 거리
+                }}
+              >
+                다음
+              </button>
             </div>
           )}
           {modalPage === 2 && (
-            <div>
-              <h2>두 번째 페이지</h2>
-              <p>여기는 두 번째 페이지입니다.</p>
-              <button onClick={handlePrevModalPage}>이전</button>
-              <button onClick={handleNextModalPage}>다음</button>
+            <div style={{ textAlign: 'center', paddingBottom: '50px' }}>
+              <video
+                src="/tutorialforPost.mp4" // public 폴더 내의 동영상 경로
+                controls
+                style={{ width: '100%', maxWidth: '600px', margin: '0 auto' }} // 크기 및 중앙 정렬
+              >
+                브라우저가 비디오 태그를 지원하지 않습니다.
+              </video>
+              <h2>포스트 를 작성하는법</h2>
+              <h3>포스트를 작성하기 위해선 채널이필요해요!</h3>
+              <p>1. 로그인후 마이페이지로 이동</p>
+              <p>2. 마이페이지 에서 채널 로 이동</p>
+              <p>3. 채널생성 버튼 클릭후 채널을 생성</p>
+              <p>
+                4. 생성한 채널에서 포스트 생성버튼을 누르면 포스트 생성완료!
+              </p>
+              <div style={{ marginTop: '20px' }}>
+                <button onClick={handlePrevModalPage}>이전</button>
+                <button
+                  onClick={handleNextModalPage}
+                  style={{ marginLeft: '10px' }}
+                >
+                  다음
+                </button>
+              </div>
             </div>
           )}
           {modalPage === 3 && (
-            <div>
-              <h2>세 번째 페이지</h2>
-              <p>여기는 세 번째 페이지입니다.</p>
+            <div style={{ textAlign: 'center', paddingBottom: '50px' }}>
+              <video
+                src="/tutorialforSubscribe.mp4" // public 폴더 내의 동영상 경로
+                controls
+                style={{ width: '100%', maxWidth: '600px', margin: '0 auto' }} // 크기 및 중앙 정렬
+              >
+                브라우저가 비디오 태그를 지원하지 않습니다.
+              </video>
+              <h2>채널 구독하는법!</h2>
+              <p>1. 마음에 드는 포스트를 클릭!</p>
+              <p>2. 포스트에 있는 채널 버튼을 누르면 팝업창이 열려요!</p>
+              <p>3. 구독하기 버튼을 누르면 구독이된답니다!</p>
+              <p>4. 상단에 잇는 구독 탭을 누르면 확인이 가능해요!</p>
+              <div style={{ marginTop: '20px' }}>
+                <button onClick={handlePrevModalPage}>이전</button>
+                <button
+                  onClick={handleNextModalPage}
+                  style={{ marginLeft: '10px' }}
+                >
+                  다음
+                </button>
+              </div>
+            </div>
+          )}
+          {modalPage === 4 && (
+            <div
+              style={{
+                position: 'relative',
+                paddingBottom: '50px',
+                textAlign: 'center',
+              }}
+            >
+              <img
+                src="/favicon.ico"
+                alt="TalentVerse Favicon"
+                style={{
+                  width: '50px',
+                  height: '50px',
+                  display: 'block',
+                  margin: '0 auto',
+                }} // 중앙 정렬
+              />
+              <h2>TalentVerse를 이용하시면서 좋은시간 보내세요!</h2>
               <button onClick={handlePrevModalPage}>이전</button>
               <button onClick={() => setModalIsOpen(false)}>닫기</button>
             </div>
